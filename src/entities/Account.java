5d513fb0b7d09274996f7e62f535cf4f58b53519
Jose Bento <jldbento@gmail.com>
2023-12-13 06:29:06 -0300

    update_bankaccount1

---
+++ src/entities/Account.java
@@ -0,0 +1,47 @@
+package entities;
+
+public class Account {
+
+    private int number;
+    private String holder;
+    private double balance;
+
+    public Account(int number, String holder) {
+        this.number = number;
+        this.holder = holder;
+    }
+
+    public Account(int number, String holder, double inicialDeposit) { //encapsulando apenas 1 ponto de alteração abaixo
+        this.number = number;
+        this.holder = holder;
+        deposit(inicialDeposit);
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+
+    public java.lang.String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(java.lang.String holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+
+    public void deposit(double amount) { // encapsulando a regra de negocio relativa a negocio dentro deste metodo
+        balance += amount;
+    }
+
+    public void withdraw(double amount){
+        balance -= amount + 5.0;
+    }
+
+}
